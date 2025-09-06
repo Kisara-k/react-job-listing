9abf32684e1323a82f2f140efda0536c9a4644f1
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-09-07 03:14:41 +0530

    jsx conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const loggedIn = true
+  return (
+  <>
+  {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+  {loggedIn && <p>Welcome Back</p>}
+  </>
+  );
 };
 
 export default App;
